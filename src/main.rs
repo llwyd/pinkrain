13afad8349deebccba0e5eed8d7be9a7650ce0d0
llwyd <tom@llwyd.io>
2023-10-09 22:33:27 +0100

    creating the framework for rainy night scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use nannou::prelude::*;
+use nannou_audio as audio;
+use nannou_audio::Buffer;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .view(view)
+        .run();
 }
+
+struct Model {}
+
+fn model(app: &App) -> Model {
+    app.new_window()
+        .size(640,480)
+        .min_size(640,480)
+        .max_size(640,480)
+        //.decorations(false)
+        .resizable(false)
+        .build()
+        .unwrap();
+
+    Model{}
+}
+
+fn update(_app:&App, _model: &mut Model, _update: Update) {}
+
+fn view( _app: &App, _model: &Model, _frame: Frame) {}
