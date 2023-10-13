23c46eb79e73322b143ad9616d06471f2954f596
llwyd <tom@llwyd.io>
2023-10-13 21:57:45 +0100

    class-ify pink noise generation into class

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use nannou::prelude::*;
 use nannou_audio as audio;
 use nannou_audio::Buffer;
-use rand::random;
+
+mod pink;
+pub use crate::pink::Pink;
 
 fn main() {
     nannou::app(model)
@@ -10,32 +12,10 @@ fn main() {
         .run();
 }
 
-#[derive(Copy, Clone)]
-struct Noise{
-    previous:f32,
-    value:f32,
-}
-
-struct Pink{
-    noise: [Noise; 15], // updated based on trailing zeros
-    white: Noise, // Updated every iteration
-    pink: f32, // Actual noise
-    
-    pink_norm: f32,
-    counter: u32,
-
-    generators: u32,
-    rollover: u32,
-}
-
 struct Model {
     _stream: audio::Stream<Pink>,
 }
 
-fn update_noise(noise:&mut Noise){
-    noise.previous = noise.value;
-    noise.value = (random::<f32>() * 2.0) - 1.0;
-}
 
 fn model(app: &App) -> Model {
     app.new_window()
@@ -47,26 +27,7 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    let mut pink = Pink{
-        noise:[Noise{previous:0.0, value:0.0}; 15],
-        white: Noise{previous:0.0, value:0.0},
-        pink: 0.0,
-        pink_norm: 0.0,
-        counter: 1,
-        generators: 15,
-        rollover: 0,
-    };
-    
-    pink.rollover = 2u32.pow(pink.generators - 1);
-
-    update_noise(&mut pink.white);
-    let generators = 15;
-    for i in 0..generators{
-        update_noise(&mut pink.noise[i]);
-        pink.pink += pink.noise[i].value;
-    }
-    pink.pink += pink.white.value;
-
+    let pink = Pink::new();
     let audio_host = audio::Host::new();
     let stream = audio_host
         .new_output_stream(pink)
@@ -79,33 +40,11 @@ fn model(app: &App) -> Model {
     Model{_stream:stream}
 }
 
-fn pink_update(pink:&mut Pink) -> f32{
-
-    let index = pink.counter.trailing_zeros();
-
-    update_noise(&mut pink.white);
-    update_noise(&mut pink.noise[index as usize]);
-
-    pink.pink = pink.pink - pink.white.previous;
-    pink.pink = pink.pink + pink.white.value;
-    
-    pink.pink = pink.pink - pink.noise[index as usize].previous;
-    pink.pink = pink.pink + pink.noise[index as usize].value;
-
-    pink.pink_norm = pink.pink / (pink.generators as f32 + 1.0);
-
-    pink.counter = pink.counter & (pink.rollover - 1); 
-    pink.counter = pink.counter + 1;
-
-    pink.pink_norm
-}
-
 fn audio(pink:&mut Pink, buffer: &mut Buffer){
     for frame in buffer.frames_mut(){
-        let pink = pink_update(pink);
+        let pink = pink.update();
         for channel in frame{
             *channel = pink;
-            //*channel = (random::<f32>() * 2.0) - 1.0;
         } 
     }
 } 
---
+++ src/pink.rs
@@ -0,0 +1,75 @@
+use rand::random;
+
+#[derive(Copy, Clone)]
+pub struct Noise{
+    previous:f32,
+    value:f32,
+}
+
+pub struct Pink{
+    noise: [Noise; 15], // updated based on trailing zeros
+    white: Noise, // Updated every iteration
+    pink: f32, // Actual noise
+    
+    pink_norm: f32,
+    counter: u32,
+
+    generators: u32,
+    rollover: u32,
+}
+
+impl Pink{
+    fn update_noise(noise:&mut Noise){
+        noise.previous = noise.value;
+        noise.value = (random::<f32>() * 2.0) - 1.0;
+    }
+    pub fn new() -> Pink{
+        let mut pink = Pink{
+            noise:[Noise{previous:0.0, value:0.0}; 15],
+            white: Noise{previous:0.0, value:0.0},
+            pink: 0.0,
+            pink_norm: 0.0,
+            counter: 1,
+            generators: 15,
+            rollover: 0,
+        };
+        
+        Self::update_noise(&mut pink.white);
+        
+        for i in 0..pink.generators{
+            Self::update_noise(&mut pink.noise[i as usize]);
+            pink.pink += pink.noise[i as usize].value;
+        }
+        pink.pink += pink.white.value;
+        
+        pink.rollover = 2u32.pow(pink.generators - 1);
+
+        pink
+    }
+    pub fn init(&mut self){
+        
+
+    }
+
+    pub fn update(&mut self) -> f32{
+
+        let index = self.counter.trailing_zeros();
+
+        Self::update_noise(&mut self.white);
+        Self::update_noise(&mut self.noise[index as usize]);
+
+        self.pink = self.pink - self.white.previous;
+        self.pink = self.pink + self.white.value;
+        
+        self.pink = self.pink - self.noise[index as usize].previous;
+        self.pink = self.pink + self.noise[index as usize].value;
+
+        self.pink_norm = self.pink / (self.generators as f32 + 1.0);
+
+        self.counter = self.counter & (self.rollover - 1); 
+        self.counter = self.counter + 1;
+
+        self.pink_norm
+    }
+}
+
