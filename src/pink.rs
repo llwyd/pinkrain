98b3fdc7f9a0bc4431dcaea37c789cd1b3fa8d0b
llwyd <tom@llwyd.io>
2023-10-15 19:41:19 +0100

    tidy up code

--- src/pink.rs
+++ src/pink.rs
@@ -11,7 +11,6 @@ pub struct Pink{
     white: Noise, // Updated every iteration
     pink: f32, // Actual noise
     
-    pink_norm: f32,
     counter: u32,
 
     generators: u32,
@@ -19,37 +18,28 @@ pub struct Pink{
 }
 
 impl Pink{
-    fn update_noise(noise:&mut Noise){
-        noise.previous = noise.value;
-        noise.value = (random::<f32>() * 2.0) - 1.0;
-    }
+    const GENERATORS: u32 = 15;
+    
     pub fn new() -> Pink{
-        let mut pink = Pink{
-            noise:[Noise{previous:0.0, value:0.0}; 15],
+        Pink{
+            noise:[Noise{previous:0.0, value:0.0}; Self::GENERATORS as usize],
             white: Noise{previous:0.0, value:0.0},
             pink: 0.0,
-            pink_norm: 0.0,
             counter: 1,
-            generators: 15,
-            rollover: 0,
-        };
-        
-        Self::update_noise(&mut pink.white);
-        
-        for i in 0..pink.generators{
-            Self::update_noise(&mut pink.noise[i as usize]);
-            pink.pink += pink.noise[i as usize].value;
+            generators: Self::GENERATORS,
+            rollover: 2u32.pow(Self::GENERATORS - 1),
         }
-        pink.pink += pink.white.value;
-        
-        pink.rollover = 2u32.pow(pink.generators - 1);
-
-        pink
     }
-   
+
+    #[allow(dead_code)] 
     fn set_counter(&mut self, new_counter:u32){
         assert!(new_counter <= self.rollover);
         self.counter = new_counter;
+    }
+    
+    fn update_noise(noise:&mut Noise){
+        noise.previous = noise.value;
+        noise.value = (random::<f32>() * 2.0) - 1.0;
     }
 
     fn get_noise_index(&self) -> u32{
@@ -71,12 +61,11 @@ impl Pink{
         self.pink = self.pink - self.noise[index as usize].previous;
         self.pink = self.pink + self.noise[index as usize].value;
 
-        self.pink_norm = self.pink / (self.generators as f32 + 1.0);
 
         self.counter = self.counter & (self.rollover - 1); 
         self.counter = self.counter + 1;
 
-        self.pink_norm
+        self.pink / (self.generators as f32 + 1.0)
     }
 }
 
