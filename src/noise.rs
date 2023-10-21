eb1968f8d67fcdd9b9b45970325224a11b6612ee
llwyd <tom@llwyd.io>
2023-10-21 23:02:48 +0100

    removed need for previous value

--- src/noise.rs
+++ src/noise.rs
@@ -2,24 +2,17 @@ use rand::random;
 
 #[derive(Copy, Clone)]
 pub struct Noise{
-    previous:f32,
     value:f32,
 }
 
 impl Noise{
     pub fn new() -> Noise{
         Noise{
-            previous: 0.0,
             value: 0.0,
         }
     }
     pub fn update(&mut self){
-        self.previous = self.value;
         self.value = (random::<f32>() * 2.0) - 1.0;
-    }
-
-    pub fn previous(&self) -> f32{
-        self.previous
     }
     
     pub fn value(&self) -> f32{
@@ -35,7 +28,6 @@ mod tests {
     fn init() {
         let n = Noise::new();
 
-        assert_eq!(n.previous(), 0.0);
         assert_eq!(n.value(), 0.0);
     }
     
@@ -43,12 +35,10 @@ mod tests {
     fn update() {
         let mut n = Noise::new();
 
-        assert_eq!(n.previous(), 0.0);
         assert_eq!(n.value(), 0.0);
 
         n.update();
         
-        assert_eq!(n.previous(), 0.0);
         assert_ne!(n.value(), 0.0);
     }
     
@@ -56,17 +46,13 @@ mod tests {
     fn update_twice() {
         let mut n = Noise::new();
 
-        assert_eq!(n.previous(), 0.0);
         assert_eq!(n.value(), 0.0);
 
         n.update();
         
-        assert_eq!(n.previous(), 0.0);
         assert_ne!(n.value(), 0.0);
 
-        let prev = n.value();
         n.update();
-        assert_eq!(n.previous(), prev);
         assert_ne!(n.value(), 0.0);
     }
 }
